d64959f8eb9dea68e52590d580331331c32a9ab7
tomroper <tomroper2@gmail.com>
2016-05-13 23:44:45 +0000

    -Renders each question in turn and compares user answers to stored correct answers

--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import {browserHistory} from 'react-router';
 
-const questions = [ 'this is q1', 'this is q2', 'this is q3' ]
+const questions = [
+  {question: 'this is q1', answer: '1'},
+  {question: 'this is q2', answer: '2'},
+  {question: 'this is q3', answer: '3'}
+];
 
-
+let numberCorrect = 0;
 
 export default class Questions extends React.Component {
 
@@ -15,12 +19,32 @@ export default class Questions extends React.Component {
   }
 
 
+  _answerSubmitted(e) {
+    e.preventDefault();
+    let userAnswer = this.refs.answer.value ;
+    let correctAnswer = questions[this.state.qCount].answer;
+
+    if(userAnswer === correctAnswer ) {
+      numberCorrect ++
+    }
+
+
+
+    if (this.state.qCount < questions.length -1){
+      this.setState({ qCount: this.state.qCount + 1})
+    } else {
+      alert('if correct answer are correct you win, if not you lose');
+    }
+  }
 
   render() {
     return(
       <div>
         <form name="questionForm">
-        <h1> {questions[qCount]} </h1>
+        <h1> {questions[this.state.qCount].question} </h1>
+        <input type="text" ref="answer" />
+        <button onClick={this._answerSubmitted.bind(this)}>submit</button>
+        {`Your score is ${numberCorrect}`}
         </form>
       </div>
     ) //return
