655bbfa4031cae5015ab129bab7f1c0758042aee
tomroper <tomroper2@gmail.com>
2016-05-13 18:43:59 +0000

    two buttons working to start test

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -7,19 +7,24 @@ export default class Welcome extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
-        //
+        clicked: false
       }
   }
 
-  _loadBeginButton() {
+  _loadBeginButton(){
+    this.setState({ clicked: true })
+  }
+
+  _loadTestScreen() {
     browserHistory.push('/test_screen')
   }
 
   render() {
     return(
-
-      <button onClick={this._loadBeginButton.bind(this)}>Start NASA test</button>
-
+      <div>
+      {!this.state.clicked ? <button onClick={this._loadBeginButton.bind(this)}>Hello!</button> :
+      <button onClick={this._loadTestScreen.bind(this)}>Start the test</button> }
+      </div>
     )
   } //render
 }//Quiz
